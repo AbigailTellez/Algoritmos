b69bc83ae2e8b0f5c47630691f63c1c50ee7dd6c
AbigailTellez <83475742+AbigailTellez@users.noreply.github.com>
2021-05-18 18:14:14 -0300

    2

--- Ordenamiento.py
+++ Ordenamiento.py
@@ -2,16 +2,16 @@ def ordenar_lista_menor_a_mayor(lista):
     return sorted(lista)
 
 def ordenar_lista_mayor_a_menor(lista):
-    listanueva=sorted(lista)
-    return listanueva.reverse()
+    return lista.sort(key= None, reverse=True)
 
 def ordenar_lista_alfabeticamente(lista):
     return sorted(lista)
 
 def ordenar_palabras_por_longitud(lista):
-    listanueva:[]
-    for i in range (0,len(lista)):
-        if len(lista[i])>len(listanueva[0]):
-            listanueva.insert( 0, lista[i])
-        else:
-            if len
+    return lista.sort(key= lambda x:len(x), reverse=True)
+
+def ordenar_lista_por_tupla(lista):
+    return lista.sort(key= lambda x: x[1], reverse=True)
+
+def ordenar_lista_por_suma_tupla(lista):
+    return lista.sort(key=lambda x: x[0]+x[1], reverse=True)
